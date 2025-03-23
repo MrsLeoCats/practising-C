9b35c6e746b4e79daf0bf105e7b5d4b30983b8fb
MrsLeoCats <85180129+MrsLeoCats@users.noreply.github.com>
2025-03-23 21:51:46 +0100

    added a counting method

--- src/main.c
+++ src/main.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
 
-int main(){
+void count(int num);
+int askInput(void);
 
-  printf("Hello World\n");
-  return 0;
-  }
+int number = 0;
+int input = 0;
+
+int main() {
+    printf("Hello World\n");
+    printf("Please input until what number you'd like to count. \n");
+    scanf("%d", &number);
+    count(number);
+    printf("\nWould you like to count again? 0 for no, 1 for yes\n");
+    askInput();
+    return 0;
+}
+
+int askInput(void) {
+    scanf("%d", &input);
+    if (input < 0 || input > 1) {
+        printf("Please choose again. 0 for no, 1 for yes\n");
+        askInput();
+    }
+    return input;
+}
+
+void count(int num) {
+    num = number;
+
+    for (int index = 1; index <= num; index++) {
+        printf("%d ", index);
+    }
+}
